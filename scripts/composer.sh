3c572076526a53ea056754fb1df73b32977fdfa8
nruest <ruestn@gmail.com>
2017-03-26 12:30:53 -0400

    Add phpcs & setup Drupal sniff file * Resolves https://github.com/Islandora-CLAW/CLAW/issues/563

--- scripts/composer.sh
+++ scripts/composer.sh
@@ -4,3 +4,11 @@ echo "Installing Composer"
 curl -sS https://getcomposer.org/installer | php
 php composer.phar install --no-progress
 mv composer.phar /usr/local/bin/composer
+
+echo "Installing phpcs"
+cd $HOME
+composer global require drupal/coder
+composer global update drupal/coder --prefer-source
+export PATH="$PATH:$HOME/.config/composer/vendor/bin"
+echo 'export PATH="$PATH:$HOME/.config/composer/vendor/bin"' >> .bashrc
+phpcs --config-set installed_paths $HOME/.config/composer/vendor/drupal/coder/coder_sniffer
