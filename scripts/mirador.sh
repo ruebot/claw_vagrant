539801713927e26a096ac15ad70091769dacd66f
Will <wwconlin@gmail.com>
2017-06-05 13:25:23 -0500

    Update mirador.sh
    unzips outside of DL function.

--- scripts/mirador.sh
+++ scripts/mirador.sh
@@ -10,15 +10,16 @@ fi
 if [ ! -f "$DOWNLOAD_DIR/master.zip" ]; then
     echo "Downloading mirador-js dependencies"
     cd "$DOWNLOAD_DIR" && { curl -LOk https://github.com/NVLI/mirador-js/archive/master.zip; cd -;}
-    cd "$DRUPAL_HOME"/web/libraries && { unzip "$DOWNLOAD_DIR"/master.zip -d . ;}
    else echo "master.zip already exists"
 fi
+
+cd "$DRUPAL_HOME"/web/libraries && { unzip "$DOWNLOAD_DIR"/master.zip -d . ;}
 
 if  [ ! -f "$DOWNLOAD_DIR/mirador-8.x-1.x-dev.zip" ]; then
     echo "Downloading mirador drupal module"
     cd "$DOWNLOAD_DIR" && { curl -LOk https://ftp.drupal.org/files/projects/mirador-8.x-1.x-dev.zip ;}
-    cd "$DRUPAL_HOME"/web/modules/contrib/ && { unzip "$DOWNLOAD_DIR"/mirador-8.x-1.x-dev.zip -d . ;}
 else echo "module already exists"
 fi
 
+cd "$DRUPAL_HOME"/web/modules/contrib/ && { unzip "$DOWNLOAD_DIR"/mirador-8.x-1.x-dev.zip -d . ;}
 cd "$DRUPAL_ROOOT" && { $DRUSH_CMD en -y mirador ;}
