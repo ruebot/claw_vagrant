184e8b7f273f7e85114c98d933f93465726faf81
Will <wwconlin@gmail.com>
2017-06-05 09:56:31 -0500

    Update mirador.sh
    conditional download and enabling, prevents repeated downloads, and uses proper $DOWNLOAD_DIR variable. I have tested it locally and it works like a charm.

--- scripts/mirador.sh
+++ scripts/mirador.sh
@@ -2,19 +2,22 @@
 echo "Installing Mirador"
 
 HOME_DIR=$1
+
 if [ -f "$HOME_DIR/islandora/configs/variables" ]; then
   . "$HOME_DIR"/islandora/configs/variables
 fi
-cd "$HOME_DIR"
 
-cd /tmp && { curl -LOk https://github.com/NVLI/mirador-js/archive/master.zip; curl -LOk https://ftp.drupal.org/files/projects/mirador-8.x-1.x-dev.zip ; cd -;}
+if [ ! -f "$DOWNLOAD_DIR/master.zip" ]; then
+    echo "Downloading mirador-js dependencies"
+    cd "$DOWNLOAD_DIR" && { curl -LOk https://github.com/NVLI/mirador-js/archive/master.zip; curl -LOk https://ftp.drupal.org/files/projects/mirador-8.x-1.x-dev.zip ; cd -;}
+    cd "$DRUPAL_HOME"/web/libraries && { unzip "$DOWNLOAD_DIR"/master.zip -d . ;}
+else echo "master.zip already exists"
+fi
 
-cd "$DRUPAL_HOME"/web/libraries && { unzip /tmp/master.zip -d . ;}
 
-rm /tmp/master.zip
-
-cd "$DRUPAL_HOME"/web/modules/contrib/ && { unzip /tmp/mirador-8.x-1.x-dev.zip -d . ;} 
-
-rm /tmp/mirador-8.x-1.x-dev.zip
-
-$DRUSH_CMD en -y mirador 
+if  [ ! -f "$DOWNLOAD_DIR/mirador-8.x-1.x-dev.zip" ]; then
+    echo "Downolading mirador drupal module"
+    cd "$DRUPAL_HOME"/web/modules/contrib/ && { unzip "$DOWNLOAD_DIR"/mirador-8.x-1.x-dev.zip -d . ;}
+    cd "$DRUPAL_ROOOT" && { $DRUSH_CMD en -y mirador ;}
+else echo "module already exists"
+fi
